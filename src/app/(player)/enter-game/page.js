9a21c787b5da8b9e67227eb2df999e33f7a3be8e
Aidan Hopper <aidanhop1@gmail.com>
2024-02-17 18:41:32 -0700

    enter game page prototype done

--- src/app/(player)/enter-game/page.js
+++ src/app/(player)/enter-game/page.js
@@ -1,5 +1,19 @@
+import Link from 'next/link'
+import { Grid, Box, Button, TextField, Typography } from '@mui/material';
+
 export default function EnterGame() {
+  //  User enters session id, player's name, and render a join game button.
+  const textbox_margin = 2
   return (
-    <>User enters session id, player's name, and render a join game button.</>
+    <Grid container
+      direction="column"
+      justifyContent="center"
+      height="100vh"
+      alignItems="center"
+    >
+      <TextField id="standard-basic" label="Session ID" variant="standard" sx={{ m: textbox_margin }} />
+      <TextField id="standard-basic" label="Name" variant="standard" sx={{ m: textbox_margin }} />
+      <Button variant="contained" size="large" sx={{ m: textbox_margin }} href="/host/decks">submit</Button>
+    </Grid>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,18 @@ import { Box, Button, TextField, Typography } from '@mui/material';
 
 export default function Home() {
 
-  const button_padding = 2
+  const button_margin = 2
 
   return (
     <main>
-      <Box 
+      <Box
         display="flex"
         justifyContent="center"
-        sx={{mx: 5}}
         alignItems="center"
-        height="100vh">
-        <Button variant="contained" size="large" sx={{m: button_padding }} href="/host/decks">Host</Button>
-        <Button variant="contained" size="large" sx={{m: button_padding }} href="/enter-game">Player</Button>
+        height="100vh"
+      >
+        <Button variant="contained" size="large" sx={{ m: button_margin }} href="/host/decks">Host</Button>
+        <Button variant="contained" size="large" sx={{ m: button_margin }} href="/enter-game">Player</Button>
       </Box>
     </main>
   )
