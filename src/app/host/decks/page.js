4c2cd1f8bd22e4237d6632fe6d6cc15bd4d675b1
kaseykartin <131495266+kaseykartin@users.noreply.github.com>
2024-02-17 16:29:26 -0800

    decks

--- src/app/host/decks/page.js
+++ src/app/host/decks/page.js
@@ -1,3 +1,16 @@
 export default function Decks() {
+
+  var deck =  [{ Question: "Which data structure uses LIFO (Last In, First Out) principle?", Answer: "Stack"}, 
+  {Question: "What is the time complexity of searching for an element in a balanced binary search tree (BST)?", Answer: "O(log n)"},
+  {Question: "In C++, what keyword is used to dynamically allocate memory?", Answer: "new"},
+  {Question: "Which of the following is a sorting algorithm that has a worst-case time complexity of O(n^2)?", Answer: "Bubble Sort"},
+  {Question: "What is the purpose of a constructor in C++?", Answer: "To initialize the values of class variables"},
+  {Question: "Which data structure is used to implement priority queues efficiently?", Answer: "Heap"},
+  {Question: "Which of the following is NOT a valid method of traversing a binary tree?", Answer: "Parallel Order"},
+  {Question: "What is a linked list?", Answer: "A data structure consisting of a sequence of nodes where each node contains a reference to the next node in the sequence"}]
+  
+  
   return <>Put the list of decks here.</>
+
 }
+
