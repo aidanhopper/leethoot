b368d53a31392937ef9b01e564031a646d8cc40a
Aidan Hopper <aidanhop1@gmail.com>
2024-02-17 18:11:55 -0700

    made buttons look nice

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
 import Link from 'next/link'
-import { Button, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 export default function Home() {
+
+  const button_padding = 2
+
   return (
     <main>
-      <Button href="/host/decks">Host</Button>
-      <Button href="/enter-game">Player</Button>
+      <Box 
+        display="flex"
+        justifyContent="center"
+        sx={{mx: 5}}
+        alignItems="center"
+        height="100vh">
+        <Button variant="contained" size="large" sx={{m: button_padding }} href="/host/decks">Host</Button>
+        <Button variant="contained" size="large" sx={{m: button_padding }} href="/enter-game">Player</Button>
+      </Box>
     </main>
   )
 }
