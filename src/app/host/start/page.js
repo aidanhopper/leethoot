78b1399904e52087cc53bda02fe4175873f020ff
Aidan Hopper <aidanhop1@gmail.com>
2024-02-17 18:58:19 -0700

    fix

--- src/app/host/start/page.js
+++ src/app/host/start/page.js
@@ -7,13 +7,13 @@ export default function Decks() {
 
   return (
     <main>
-      <Box 
+      <Box
         display="flex"
         justifyContent="center"
-        sx={{mx: 5}}
+        sx={{ mx: 5 }}
         alignItems="center"
         height="100vh">
-        <Button variant="contained" size="large" sx={{m: button_padding }} href="/game">Start Game</Button>
+        <Button variant="contained" size="large" sx={{ m: button_padding }} href="/game">Start Game</Button>
       </Box>
     </main>
   )
