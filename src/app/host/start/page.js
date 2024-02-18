bde9ccb18dfd50cf47f22f3fbfc7e4db1598459e
kaseykartin <131495266+kaseykartin@users.noreply.github.com>
2024-02-17 17:32:36 -0800

    button in /host/start/page.js

--- src/app/host/start/page.js
+++ src/app/host/start/page.js
@@ -1,3 +1,17 @@
 export default function Decks() {
-  return <>Start game button goes here.</>
+
+  const button_padding = 2
+
+  return (
+    <main>
+      <Box 
+        display="flex"
+        justifyContent="center"
+        sx={{mx: 5}}
+        alignItems="center"
+        height="100vh">
+        <Button variant="contained" size="large" sx={{m: button_padding }} href="/game">Start Game</Button>
+      </Box>
+    </main>
+  )
 }
