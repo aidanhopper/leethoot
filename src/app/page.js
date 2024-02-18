87dea7eaff0422951d3e528ff3429d7b583d6fe3
Benjamin Swanzey <swanzeyb2001@gmail.com>
2024-02-17 16:05:20 -0800

    Add host or player links

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
+      <Link href="/host/decks">Host</Link>
+      <Link href="/enter-game">Player</Link>
     </main>
-  );
+  )
 }
